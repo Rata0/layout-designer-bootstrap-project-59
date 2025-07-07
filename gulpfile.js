ebd8da83dad186db99d406de43272f607966a205
Rata0 <nekon689@gmail.com>
2025-07-07 10:18:46 +0300

    Added svg-sprite processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-const { src, dest, parallel } = require('gulp')
+const { src, dest, parallel, watch } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const pug = require('gulp-pug')
 const browserSync = require('browser-sync').create()
+const svgSprite = require('gulp-svg-sprite');
 
 const browserSyncJob = () => {
   browserSync.init({
@@ -16,7 +17,7 @@ const buildPug = () => {
   console.log('Компиляция Pug')
 
   return src('app/pages/**/*.pug')
-    .pipe(pug())
+    .pipe(pug({ pretty: true }))
     .pipe(dest('build/'))
     .pipe(browserSync.stream())
 }
@@ -30,5 +31,28 @@ const buildSass = () => {
     .pipe(browserSync.stream())
 }
 
+const config = {
+  mode: {
+    stack: {
+      sprite: "../sprite.svg"
+    }
+  },
+};
+
+const buildSvgSprite = () => {
+  console.log('Компиляция SVG-спрайта');
+
+  return src('app/assets/icons/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(dest('build/assets/icons/'));
+}
+
+const buildJpg = () => {
+  console.log('Копирование JPG...');
+  
+  return src('app/assets/images/*.jpg')
+    .pipe(dest('build/assets/images/'))
+};
+
 exports.server = browserSyncJob
-exports.build = parallel(buildSass, buildPug)
+exports.build = parallel(buildSass, buildPug, buildSvgSprite, buildJpg)
